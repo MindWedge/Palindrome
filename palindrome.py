4f5fdafd844bff4ba28ba86d5e351bfeb6527788
xane <xane@pop-os.localdomain>
2023-02-14 10:30:36 -0500

    Check length to pass 2nd Test

--- palindrome.py
+++ palindrome.py
@@ -2,5 +2,9 @@ from collections import deque
 def is_palindrome(word):
     if not isinstance(word,str):
         raise ValueError("Input value is not a string")
-
+	
+	# checking length to be sure more then zero
+    if len(word) ==  0:
+        return False
+        
     return True
